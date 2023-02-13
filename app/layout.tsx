ed96e7a09c1bd813cfd37dd5a86c5cee92a391e1
JJ Kasper <jj@jjsweb.site>
2023-02-13 15:47:51 -0800

    Update to latest version of Next.js (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 export const metadata = {
-  title: 'Next.js on the Edge',
-  description: 'HTML, dynamically rendered in a city near you',
+  title: "Next.js on the Edge",
+  description: "HTML, dynamically rendered in a city near you",
   twitter: {
-    card: 'summary_large_image',
-    title: 'Next.js on the Edge',
-    description: 'HTML, dynamically rendered in a city near you',
-    creator: '@nextjs',
+    card: "summary_large_image",
+    title: "Next.js on the Edge",
+    description: "HTML, dynamically rendered in a city near you",
+    creator: "@nextjs",
     images: {
-      url: '/static/og/card.png',
-      alt: 'The Vercel and Next.js logos',
+      url: "/static/og/card.png",
+      alt: "The Vercel and Next.js logos",
     },
   },
   icons: {
-    icon: '/static/favicon/favicon-32x32.png',
-    shortcut: '/static/favicon/favicon.ico',
-    apple: '/static/favicon/apple-touch-icon.png',
+    icon: "/static/favicon/favicon-32x32.png",
+    shortcut: "/static/favicon/favicon.ico",
+    apple: "/static/favicon/apple-touch-icon.png",
   },
-  themeColor: '#FFF',
+  themeColor: "#FFF",
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : "http://localhost:3000"
+  ),
 };
 
 export default function RootLayout({
